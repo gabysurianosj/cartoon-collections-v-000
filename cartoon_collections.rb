ca33a2e58bb23bdf66881aec44717375e90d6ae8
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 14:01:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,15 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls) 
+  calls.any? do |call|
+    call.length > 4 
+  end 
 end
 
 def find_the_cheese# code an argument here
