b812165f5d57475640a15150c7241be9fa027985
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 13:58:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
-def roll_call_dwarves(name)
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    index += 1 
+    puts #{index}. #{name}
+  end 
 end
 
 def summon_captain_planet# code an argument here
